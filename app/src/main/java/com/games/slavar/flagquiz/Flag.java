e4fb19078db3654d21792ae4776896796ed83c2a
Slavar <slava.rubej@gmail.com>
2015-12-29 21:45:08 +0200

    1st update

---
+++ app/src/main/java/com/games/slavar/flagquiz/Flag.java
@@ -0,0 +1,29 @@
+package com.games.slavar.flagquiz;
+
+/**
+ * Created by Slavar on 12/29/2015.
+ */
+public class Flag {
+
+    private String name;
+    private String pictureUrl;
+
+    public Flag() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+}
