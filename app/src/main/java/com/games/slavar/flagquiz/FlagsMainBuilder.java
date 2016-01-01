4c32d5bf29a2b3c3bcc927ad25aeba9b9964fcd4
komissar2007 <slava.rubej@gmail.com>
2016-01-02 00:19:53 +0200

    Stage adding

--- app/src/main/java/com/games/slavar/flagquiz/FlagsMainBuilder.java
+++ app/src/main/java/com/games/slavar/flagquiz/FlagsMainBuilder.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by Slavar on 12/31/2015.
@@ -37,6 +38,9 @@ public class FlagsMainBuilder {
                 Log.d(getClass().toString(), "id:" + flag.getId() + ", " + "fileName:" + flag.getFileName() + ", " + "Name:" + flag.getName());
                 flagArrayList.add(flag);
             }
+            
+            shuffleArray(flagArrayList);
+            
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,4 +50,25 @@ public class FlagsMainBuilder {
         }
 
     }
+
+    private void shuffleArray(ArrayList<Flag> flagArrayList) {
+
+        Random rnd = new Random();
+        for (int i = flagArrayList.size() - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            Flag a = flagArrayList.get(index);
+            flagArrayList.set(index, flagArrayList.get(i));
+            flagArrayList.set(i,a);
+        }
+        for (Flag flagId:flagArrayList)
+        {
+            System.out.println(flagId.getId());
+
+            Log.d(getClass().toString(), String.valueOf(flagId.getId()));
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/games/slavar/flagquiz/Stage.java
@@ -0,0 +1,10 @@
+package com.games.slavar.flagquiz;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Created by Slava on 02-1-16.
+ */
+public class Stage {
+}
