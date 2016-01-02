8a53b1cc9b3f04c655226ddced115f0791b0f865
komissar2007 <slava.rubej@gmail.com>
2016-01-02 08:50:43 +0200

    StageBuilder and Stage classes

--- app/src/main/java/com/games/slavar/flagquiz/Stage.java
+++ app/src/main/java/com/games/slavar/flagquiz/Stage.java
@@ -1,10 +1,28 @@
 package com.games.slavar.flagquiz;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 /**
  * Created by Slava on 02-1-16.
  */
 public class Stage {
+    final int numberOfAnswers = 4;
+    private Flag question;
+    private Flag[] answers = new Flag[4];
+
+    public Flag getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Flag question) {
+        this.question = question;
+    }
+
+    public Flag[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Flag[] answers) {
+        this.answers = answers;
+    }
 }
---
+++ app/src/main/java/com/games/slavar/flagquiz/StageBuilder.java
@@ -0,0 +1,7 @@
+package com.games.slavar.flagquiz;
+
+/**
+ * Created by Slava on 02-1-16.
+ */
+public class StageBuilder {
+}
