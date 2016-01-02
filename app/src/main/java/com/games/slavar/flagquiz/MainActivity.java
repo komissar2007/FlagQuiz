bf550e6659a5c01a07716ba1aa85144eb8cbe7b1
komissar2007 <slava.rubej@gmail.com>
2016-01-02 14:00:46 +0200

    prepareStage

--- app/src/main/java/com/games/slavar/flagquiz/MainActivity.java
+++ app/src/main/java/com/games/slavar/flagquiz/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton flagImage;
     private Button[] answerButton = new Button[4];
-    
+    private ArrayList<Stage> stageArrayList = new ArrayList<Stage>() ;
+    int stage = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FlagsMainBuilder flagMainBuilder = new FlagsMainBuilder();
         ArrayList<Flag> flagsArray = flagMainBuilder.buildFlagArray(getApplicationContext());
-        StageBuilder stageArrayList = new StageBuilder(flagsArray);
-        stageArrayList.populateQuestions();
-
+        StageBuilder stageBuilder = new StageBuilder(flagsArray);
+        stageBuilder.populateQuestions();
+        stageArrayList = stageBuilder.getStageArrayList();
         flagImage = (ImageButton) findViewById(R.id.flagImageButton);
+        answerButton[0] = (Button) findViewById(R.id.resultButtonA);
+        answerButton[1] = (Button) findViewById(R.id.resultButtonB);
+        answerButton[2] = (Button) findViewById(R.id.resultButtonC);
+        answerButton[3] = (Button) findViewById(R.id.resultButtonD);
+    }
+
+    public void prepareStage()
+    {
+
     }
 }
